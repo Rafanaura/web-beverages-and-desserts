774200dc5c68d478ca9d2af36d89f968db38716d
Rafanaura <88173145+Rafanaura@users.noreply.github.com>
2021-10-21 07:59:17 +0700

    Add files via upload

---
+++ beverages.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="beverages.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+</head>
+<body>
+    <div class="menu">
+        <h1><b>Beverages</b></h1>
+    </div>
+<section class="beverages" id="menu">
+
+        <div class="container">
+            <main class="grid">
+                <article>
+                    <img src="image/cappuccino.jpg" alt="" width="300px" height="250px">
+                <div class="teks">
+                    <h3>Cappuccino</h3>
+                    <p>this is cappuccino.</p>
+                </div>
+                </article>
+
+                <article>
+                <img src="image/lemon squash.jpg" alt="" width="300px" height="230px">
+                <div class="teks">
+                   <h3>Squash</h3>
+                    <p>this is lemon squash. </p>
+                </div>
+                </article>
+
+                <article>
+                <img src="image/milkshake.png" alt="" width="300px" height="250px">
+                <div class="teks">
+                   <h3>Milk Shake</h3>
+                    <p>this is milk shake</p>
+                </div>
+                </article>
+
+                <article>
+                <img src="image/matcha uwu.jpg" alt="" width="300px" height="210px">
+                <div class="teks">
+                    <h3> Milk Tea</h3>
+                    <p>this is matcha milk tea.</p>
+                </div>
+                </article>
+
+                <article>
+                <img src="image/juice.jpg" alt="" width="300px" height="250px">
+                <div class="teks">
+                   <h3>Fruit Juice</h3>
+                    <p>this is fruit juice.</p>
+                </div>
+                </article>
+
+                <article>
+                <img src="image/coffee.jpg" alt="" width="300px" height="250px">
+                <div class="teks">
+                   <h3>Coffee</h3>
+                    <p>this is coffee.</p>
+                </div>
+                </article>
+                </main>
+        </div>
+        </section>
+</body>
+</html>
---
+++ desserts.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="desserts.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+</head>
+<body>
+<div class="menu">
+        <h1><b>MENU</b></h1>
+    </div>
+<section class="desserts1" id="menu">
+        
+        <div class="container">
+            <main class="grid">
+                <article>
+                    <img src="image/puding.jpg" alt="" width="200px" height="250px">
+                <div class="teks">
+                    <h3>Puding</h3>
+                    <p>this is puding.</p>
+                </div>
+                </article>
+                
+                <article>
+                    <img src="image/donat.jpg" alt="" width="200px" height="250px">
+                <div class="teks">
+                    <h3>Donut</h3>
+                    <p>this is donut.</p>
+                    </div>
+                </article>
+
+                <article>
+                    <img src="image/cake.jpg" alt="" width="200px" height="250px">
+                    <div class="teks">
+                        <h3>Cake</h3>
+                        <p>this is cake.</p>
+                    </div>
+                </article>
+
+                <article>
+                <img src="image/waffle.png" alt="" width="200px" height="250px">
+                <div class="teks">
+                    <h3>Waffle</h3>
+                    <p>this is waffle.</p>
+                </div>
+                </article>
+
+                <article>
+                    <img src="image/cupcake.jpg" alt="" width="200px" height="250px">
+                <div class="teks">
+                    <h3>Cupcake</h3>
+                    <p>this is cupcake.</p>
+                </div>
+                </article>
+
+                <article>
+                    <img src="image/ice cream.jpg" alt="" width="200px" height="250px">
+                <div class="teks">
+                    <h3>Ice Cream</h3>
+                    <p>this is ice cream.</p>
+                </div>
+                </article>
+            </main>
+        </div>
+    </section>
+</body>
+</html>
